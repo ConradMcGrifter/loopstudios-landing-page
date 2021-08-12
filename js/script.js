d1624006397e4e2f4a087dff3681d7bc68f5b069
skf <79630413+ConradMcGrifter@users.noreply.github.com>
2021-08-11 23:55:51 -0400

    finished with the header/nav/hero section
    added visibility property to the nav wrap so that I could apply a transition on the background color so it will work on mobile.
    
    I have had problems with css animations only running once on mobile and not repeating the animation. I found that transition property works best because it will work over and over again

--- js/script.js
+++ js/script.js
@@ -2,8 +2,12 @@ const mobileNavButton = document.getElementById("hamburger-btn");
 const navUlWrap = document.querySelector(".nav-ul--wrap");
 const navUl = document.querySelector(".nav__ul");
 const BODY = document.querySelector("body");
+const closeImage = document.getElementById("x-img");
+const hamburgerImage = document.getElementById("hamburger-img");
 
 mobileNavButton.addEventListener("click", () => {
+    hamburgerImage.classList.toggle("display");
+    closeImage.classList.toggle("display");
     navUlWrap.classList.toggle("active");
     navUl.classList.toggle("active");
     BODY.classList.toggle("display");
